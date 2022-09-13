beebb06a109e2da2d8ae18cb19134261bda8b4b1
Thalles Bhering <thbg1@hotmail.com>
2022-09-12 22:08:51 -0300

    Atividade 4 senai

---
+++ ECR1/atividade4.js
@@ -0,0 +1,9 @@
+let idade = 18;
+let listaDePessoas = 100;
+
+if (idade >= 18 && listaDePessoas < 100) {
+    console.log("Bem vindo a festa!")
+}else{
+    console.log("Erro, infelizmente não foi possível fazer seu cadastro")
+}
+
